2b0b977b0c42e15dd61d3fb9391a083b656288af
opilane <opilane@HP-CZC2349HT2>
2024-03-25 13:23:21 +0200

    Add project files.

---
+++ tabbed_pages/Veebruar.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tabbed_pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Veebruar : ContentPage
+    {
+        public Veebruar()
+        {
+            InitializeComponent();
+        }
+    }
+}
